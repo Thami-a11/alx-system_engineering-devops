37a86eac3d8972858b58df84fa998eafda0bf4f2
Thami-a11 <78639094+Thami-a11@users.noreply.github.com>
2024-09-20 21:00:31 +0200

    Make Exe

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-"""Module for task 1"""
+"""
+    Uses the reddit API for get the 10 hot posts
+"""
+import requests
+from sys import argv
 
 
 def top_ten(subreddit):
-    """Queries the Reddit API and returns the top 10 hot posts
-    of the subreddit"""
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
+    """Get the 10 host posts"""
+    url_sred_inf = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url_sred_inf += "?limit=10"
+    headers = {'user-agent': 'request'}
+    response = requests.get(url_sred_inf, headers=headers,
                             allow_redirects=False)
-    if sub_info.status_code >= 300:
-        print('None')
-    else:
-        [print(child.get("data").get("title"))
-         for child in sub_info.json().get("data").get("children")]
+    if str(response) != "<Response [200]>":
+        print(None)
+        return
+    r_json = response.json()
+    hot_posts_json = r_json.get("data").get("children")
+    top_10_posts = ""
+    for post in hot_posts_json:
+        top_10_posts += post.get("data").get("title") + "\n"
+    print(top_10_posts, end="")
